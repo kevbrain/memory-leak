7369b803a1fba2269de1126eab76e195577e6df2
'ksc' <'ksc@example.com';>
2022-06-19 17:18:55 +0200

    init

--- src/main/java/com/its4u/Application.java
+++ src/main/java/com/its4u/Application.java
@@ -1,15 +1,27 @@
 package com.its4u;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.its4u.AllocMemoryBean.Key;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+		Map<Key, String> map = new HashMap<Key, String>(1000);   
+	    int counter = 0;
+		for (int i=0;i<10000000;i++) {
+		      map.put(new Key("dummyKey"), "value");
+		      counter++;		       
+		 }
+		 System.out.println("1GB Allocated Memory");
 		 SpringApplication.run(Application.class, args);
-
+		 
 	}
 	
 }
